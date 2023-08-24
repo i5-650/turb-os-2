ae8cc8fcc532cbe8ea1dcfc251e39f4b8231d2f7
loic-prn <l.perrin@proton.me>
2023-08-24 11:28:12 +0200

    Adding comments 📄

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,15 @@ fn panic(_info: &PanicInfo) -> ! { // ! = "never type"
 pub extern "C" fn _start() -> ! {
 	let vga_buffer = 0xb8000 as *mut u8;
 
+	// for each letter of HELLO string
 	for (i, &byte) in HELLO.iter().enumerate() {
+		// unsafe because the compiler cannot be sure the raw pointer is valid
+		// so by saying unsafe, we tell the compiler that we (coder) are sure it's valid
 		unsafe {
+			// the letter byte 
 			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+			// the color byte
+			*vga_buffer.offset(i as isize * 2 + 1) = 0xf; // 0xf = white
 		}
 	}
 
