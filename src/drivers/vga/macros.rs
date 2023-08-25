a89f2db3f48e2a94af1b43f145b2a35327e2c27d
i5-650 <77734008+i5-650@users.noreply.github.com>
2023-08-26 00:47:22 +0200

    Cleaning up 🧹

---
+++ src/drivers/vga/macros.rs
@@ -0,0 +1,26 @@
+use core::fmt::{Write, Arguments, Result};
+use crate::drivers::vga::vga_driver::*;
+
+impl Write for VgaWriter {
+	fn write_str(&mut self, s: &str) -> Result {
+		self.write_str(s);
+		return Ok(());
+	}
+}
+
+
+#[macro_export]
+macro_rules! print {
+	($($arg:tt)*) => (crate::drivers::vga::macros::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+	() => ($crate::print!("\n"));
+	($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: Arguments) {
+	let _ = WRITER.lock().write_fmt(args);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,16 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod drivers;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { // ! = "never type"
+fn panic(_info: &PanicInfo) -> ! { // ! = "never" type
+	println!("{}", _info);
 	loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	let vga_buffer = 0xb8000 as *mut u8;
-
-	// for each letter of HELLO string
-	for (i, &byte) in HELLO.iter().enumerate() {
-		// unsafe because the compiler cannot be sure the raw pointer is valid
-		// so by saying unsafe, we tell the compiler that we (coder) are sure it's valid
-		unsafe {
-			// the letter byte 
-			*vga_buffer.offset(i as isize * 2) = byte;
-			// the color byte
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xf; // 0xf = white
-		}
-	}
-
+	println!("Hello, 2+2={}", 4);
 	loop {}
 }
